9ed966b36a81453329d8a379812453f768397a5b
LEVAN <LEVAN@JOTA>
2022-06-11 17:35:08 -0500

    Agregar archivos de proyecto.

---
+++ TESTFIBONACCI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TESTFIBONACCI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TESTFIBONACCI")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5556f775-d230-4695-bf83-323bcc9a0365")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TESTFIBONACCI/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TESTFIBONACCI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = Fibonacci_Ejercicio2.Program.fibonacci(10);
+            Assert.AreEqual(9, result);
+        }
+    }
+}
